48814d098ee53bfd8ef8a8902417adf3caf3cab7
Ernesto Valecillos <evalecillos@gmail.com>
2021-11-13 22:13:04 -0600

    fixed authentication

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // get all users
 router.get("/", (req, res) => {
@@ -88,7 +89,7 @@ router.post("/", (req, res) => {
         });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
     Post.update(
         {
             title: req.body.title,
@@ -113,7 +114,7 @@ router.put("/:id", (req, res) => {
         });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id,
